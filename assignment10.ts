c46bbb4b9f7180cf309ee10cacee1e621de31509
Deepak Saini <deepak.saini@binarysemantics.com>
2026-02-24 16:55:20 +0530

    assignment10

---
+++ assignment10.ts
@@ -0,0 +1,19 @@
+export function maxProfit(prices: number[]): number {
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        for (let j = i + 1; j < prices.length; j++) {
+            const profit = prices[j]! - prices[i]!;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+    }
+
+    return maxProfit;
+}
+
+console.log(maxProfit([7, 1, 5, 3, 6, 4])); 
+console.log(maxProfit([7, 6, 4, 3, 1]));    
+
